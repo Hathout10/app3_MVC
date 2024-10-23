39cfe915be63c9fae1629b4acb0592a83efec7a9
Mohamed Hathout <ma7755111@gmail.com>
2024-10-24 01:49:01 +0300

    update01

--- app3_PL/wwwroot/js/site.js
+++ app3_PL/wwwroot/js/site.js
@@ -3,9 +3,26 @@
 
 // Write your JavaScript code.
 
-let element = Document.GetElementById("id");
+let element = document.getElementById("id");
 
 element.addEventListener("Keyup", () => {
 
     //send request to BackEnd
+
+
+    // Creating Our XMLHttpRequest object 
+    let xhr = new XMLHttpRequest();
+
+    // Making our connection  
+    let url = 'https://localhost:44347/Employee/Index?inputSearch=${element.value}';
+    xhr.open("Post", url, true);
+
+    // function execute after request is successful 
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    // Sending our request 
+    xhr.send();
 });
